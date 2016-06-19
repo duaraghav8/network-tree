3209294afea061d19b281044c381d0e81f3e524a
duaraghav8@gmail <duaraghav8@gmail.com>
2016-06-19 13:47:59 +0530

    Enhanced API

--- examples/generate_d3_json.js
+++ examples/generate_d3_json.js
@@ -6,22 +6,28 @@
 
 var networkTree = require ('network-tree'),
 	app = require ('express') ();
+
 var tree, treeCache,
 	domain = 'http://raghavdua.com/';
 
 app
-	.get ('/tree', function (req, res) {
+	.get ('/tree/:format', function (req, res) {
 		if (treeCache) return res.json (treeCache);
 
 		tree = new networkTree (domain);
 		tree
 			.get ({
-				format: 'd3'		//d3,sigma
+				formats: ['d3', 'sigma']	//produce the sitemap json for both d3.js tree and sigma.js tree
 			})
-			.then (function (json) {
-				treeCache = json;
-				res.json (json);
+
+			.then (function (jsons) {
+				var requestedFormat = req.params.format;
+				treeCache = jsons;
+
+				if (Object.keys (jsons).indexOf (requestedFormat) === -1) return res.sendStatus (404);
+				res.json (jsons [requestedFormat]);
 			})
+
 			.catch (function (err) {
 				console.log (err);
 				res.sendStatus (500);
@@ -29,3 +35,14 @@ app
 	})
 
 	.listen (3000);
+
+/**
+ *	Output = {
+ *		"d3": {
+ 			<SITEMAP JSON in d3.js format>
+ 		},
+ *		"sigma": {
+ 			<SITEMAP JSON in sigma.js format>
+ 		}
+ *	}
+ */
